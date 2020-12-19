83dfd9a59c756cbd19775e148fa92ca3ed036645
Chidiebere Okpala <okpalacollins4@gmail.com>
2020-12-19 17:18:49 -0600

    Update State - 1
    Here we have a component(DisplayQuizz) that updates its own internal state
    
    For this component, due to the fact that generating and displaying questions does not depend on any data from the parent, we have this component define its own state at initialization(from its constuctor) and this componets also alters its own internal state(so the state is not alterned by the parent or closest ancestor component being Apps)
    
    Summary: At this point, we have the UI change to a new question when the user clickes the true or false button

--- src/DisplayQuizz.js
+++ src/DisplayQuizz.js
@@ -23,6 +23,24 @@ class DisplayQuizz extends React.Component{
       const proposedAnswer = Math.floor(Math.random() * 3) + (value1 + value2 + value3);
       return [value1, value2, value3, proposedAnswer];
     };
+
+    //This wil be used to set the next question. Note that we did not use the previousState() value to set the next state because next question does not depend on the previous question
+    //The function updateState takes in a new question then updates the UI with the new question by calling the setState() method
+    updateState = newValuesArray => {
+        this.setState(currState => ({
+          value1: newValuesArray[0],
+          value2: newValuesArray[1],
+          value3: newValuesArray[2],
+          proposedAnswer: newValuesArray[3],
+        }));
+    };
+
+    //This method will be called when the user clickes the true or false button
+    //It update the UI with a new question by calling the updateState
+    handleAnswer = event => {
+        const newValuesArray = this.makeNewQuestion(); //gets a new question
+        this.updateState(newValuesArray); //updates the UI
+    };
     
     render() {
       const { value1, value2, value3, proposedAnswer } = this.state;
@@ -33,10 +51,10 @@ class DisplayQuizz extends React.Component{
           <div className="equation">
             <p className="text">{`${value1} + ${value2} + ${value3} = ${proposedAnswer}`}</p>
           </div>
-          <button name="true">
+          <button name="true" onClick={this.handleAnswer}>
             True
           </button>
-          <button name="false">
+          <button name="false" onClick={this.handleAnswer}>
             False
           </button>
         </div>
